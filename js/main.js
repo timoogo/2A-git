6b68adaee63a9a6d1e40f13b9adeacd302f94ffa
TheMadSandman <martindeboutray@hotmail.fr>
2020-10-08 17:41:04 +0200

    🔨 Added main.js with the members variable to contain popup info

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// ---- VARIABLES ----
+const members = [
+    {
+        name: 'Thimothée "Timoogo" G.',
+        profilePicture: { src: "img/timothee_g.jpg", alt: "Image profil Thimotée"},
+        miniBio: "*Insert Timoogo's mini-bio here*",
+        fullBio: "*Insert Timoogo's full-bio here*"
+    },
+    {
+        name: 'Alexandre "ImWallAshing" V.',
+        profilePicture: { src: "img/imagelove-lies-2-pika.png", alt: "Image de profil Alexandre (elle est meilleurs que les autres)"},
+        miniBio: "*Insert ImWallAshing's mini-bio here*",
+        fullBio: "*Insert ImWallAshing's full-bio here*"
+    },
+    {
+        name: 'Maxime "SveRKeR" A.',
+        profilePicture: { src: "img/sverker.png", alt: "Image profil Maxime"},
+        miniBio: "*Insert SveRKeR's mini-bio here*",
+        fullBio: "*Insert SveRKeR's full-bio here*"
+    },
+    {
+        name: 'Martin "MadSandman" B.',
+        profilePicture: { src: "img/martin_b.png", alt: "Image de profil Martin"},
+        miniBio: "*Insert MadSandman's mini-bio here*",
+        fullBio: "*Insert MadSandman's full-bio here*"
+    }
+]
